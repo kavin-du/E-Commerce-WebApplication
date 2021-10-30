7389d8b685d33fc876add787ea4bc7488ed01b22
kavin-du <ukdkcm@gmail.com>
2021-10-30 12:08:46 +0530

    add category

--- src/app/COMPONENTS/add-category/add-category.component.ts
+++ src/app/COMPONENTS/add-category/add-category.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, Validators, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-add-category',
@@ -7,9 +8,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddCategoryComponent implements OnInit {
 
-  constructor() { }
+  public categoryFormGroup: FormGroup = this.fb.group({
+    categoryName: ['', Validators.required],
+  });
+  
+  
+  constructor(private fb: FormBuilder) { 
 
+  }
+  
   ngOnInit(): void {
+  }
+
+  onSubmit(): void {   
+    console.log(this.categoryFormGroup.controls.categoryName.value);
+    
+  }
+
+  clearForm(): void {
+    this.categoryFormGroup.reset();
+    // Object.keys(this.registerFormGroup.controls)
+    // .forEach(key => {
+    //   this.registerFormGroup.get(key)?.setErrors(null);
+    // });
+        
   }
 
 }
